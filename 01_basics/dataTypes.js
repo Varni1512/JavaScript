0ca6f6129136e007ef5f1e456b9b36fddfee1a3d
Varni1102 <varnikumar1512@gmail.com>
2026-02-05 22:52:33 +0530

    first commit

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3+3) //we are using nodejs, not browser
+
+let name = "varni"
+let age = 15
+let isLoggedIn = false
+
+// number => 2^53
+// bigint => for trading and all
+// string => ""
+// boolean => true/false
+// null => standalone value
+//undefined => 
+// symbol => unique
+
+
+// object 
+
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
+
---
+++ 01_basics/variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accountEmail = "varni152@gmail.com"
+var accountPassword = "123456"
+accountCity = "Gujrat"
+
+// accountId = 2 //not allowed
+
+accountEmail= "hc@hc.com"
+accountPassword = "123456789"
+accountCity = "Goa"
+
+
+/* Prefer not to use var because in block scope and functional scope */
+
+console.table([accountEmail,accountId,accountPassword,accountCity])
